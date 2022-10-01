64fee7f20b54156cf3831a3ce4ce7accaca39911
Kian Boon <kianboon@yctan.com>
2022-10-01 12:54:14 -0700

    Basic Front Page Design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import "./App.css";
+
 function App() {
+  const onSubmit = () => {
+    console.log("ay");
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div className="main-page">
+      <div className="header-container">
+        <img src="/blue_bikes_tracker_logo.png" alt="logo"/>
+        <h1>Blazingly Fast machine learning driven application</h1>
+        <p>Doing stuff like never before</p>
+      </div>
+
+      <div className="form-container">
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="input 1"/>
+          <input type="text" placeholder="input 2"/>
+          <input type="text" placeholder="input 3"/>
+
+          <input type="submit" value="Run Prediction!"/>
+        </form>
+      </div>
+    </div>
   );
 }
 
